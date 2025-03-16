71e2622e2fdab956deb9e5a45e5a01b86ae2a0cc
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-03-16 15:19:29 +0900

    fix frontend作り直し

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,2 @@
-<h1>11Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
