b3d42e1fddf966aad2ef8d452f98af164abb6917
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-06-11 23:42:01 +0900

    feat いったんfrontend初期にする

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,2 @@
-<script lang="ts">
-  import Logo from "$lib/assets/logo_v1.png";
-  import NormalButton from "$lib/components/atom/button/NormalButton.svelte";
-</script>
-
-<h1 class="text-2xl m-4 text-center">umi.mikan</h1>
-
-<div class="flex justify-center">
-  <img alt="logo" src={Logo} loading="lazy" class="w-24 h-24" />
-</div>
-<NormalButton
-  title="login"
-  url="/login"
-  bgColorVariable="blue"
-  textColorVariable="black"
-/>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
