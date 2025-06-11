ed3496ce861421e390d7b9a75c487e00f9e1d341
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-06-12 00:28:49 +0900

    alias追加

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,11 +8,20 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		alias: {
+			"$src/*": "src/*",
+			"$apiSchema/*": "src/apiSchema/*",
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		files: {
+			hooks: {
+				server: "src/lib/hooks/hooks.server.ts",
+			},
+		},
+	},
 };
 
 export default config;
