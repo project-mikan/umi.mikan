057d9cf091bca72c258d481b857871197b88e4de
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-09-08 21:10:48 +0900

    wip いったんdbまで

--- schema/2100_diary_months.sql
+++ schema/2100_diary_months.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS diary_months (
     id UUID PRIMARY KEY,
-    diary_id UUID REFERENCES users(id) NOT NULL,                           
+    user_id UUID REFERENCES users(id) NOT NULL,                           
     content TEXT NOT NULL,
     date DATE NOT NULL,
     created_at BIGINT NOT NULL,
     updated_at BIGINT NOT NULL,
-    CONSTRAINT unique_user_date UNIQUE (user_id, date) -- ユーザーごとに日付は一意
+    CONSTRAINT unique_user_date_months UNIQUE (user_id, date) -- ユーザーごとに日付は一意
 );
 
-CREATE INDEX index_diaries_user_id_and_date ON diaries (user_id, date);
+CREATE INDEX index_diary_months_user_id_and_date ON diary_months (user_id, date);
 
