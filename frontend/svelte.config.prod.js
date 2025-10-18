1ffddaf5c19c0db9727d57344c951ccc7092a3f1
usuyuki <63891531+usuyuki@users.noreply.github.com>
2025-10-18 11:19:03 +0900

    fix build warn#

--- frontend/svelte.config.prod.js
+++ frontend/svelte.config.prod.js
@@ -13,9 +13,19 @@ const config = {
 			"$apiSchema/*": "src/apiSchema/*",
 		},
 		adapter: adapter(),
-		files: {
-			hooks: {
-				server: "src/lib/hooks/hooks.server.ts",
+		csp: {
+			mode: "hash",
+			directives: {
+				"default-src": ["self"],
+				"script-src": ["self"],
+				"style-src": ["self", "unsafe-inline"],
+				"img-src": ["self", "data:", "blob:"],
+				"font-src": ["self"],
+				"connect-src": ["self", "http://localhost:2001", "http://backend:8080"],
+				"form-action": ["self"],
+				"frame-ancestors": ["none"],
+				"object-src": ["none"],
+				"base-uri": ["self"],
 			},
 		},
 	},
