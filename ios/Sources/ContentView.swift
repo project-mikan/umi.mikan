b375679a8f976adf071d97d74dae30477380028f
usuyuki <63891531+usuyuki@users.noreply.github.com>
2026-06-21 15:51:05 +0900

    feat 基本構成

---
+++ ios/Sources/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  umi.mikan
+//
+//  Created by usuyuki on 2026/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Sources/umi_mikanApp.swift
@@ -0,0 +1,17 @@
+//
+//  umi_mikanApp.swift
+//  umi.mikan
+//
+//  Created by usuyuki on 2026/06/20.
+//
+
+import SwiftUI
+
+@main
+struct umi_mikanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
